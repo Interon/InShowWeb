ab084544f56b5d0352a8a5f46701d4932ab61a35
Anton Oosthuizen <anton@interon.co.za>
2016-04-12 16:54:08 +0200

    Initial Commit

---
+++ App_Code/Extensions/MyExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for _Extensions
+/// </summary>
+namespace ExtensionMethods
+{
+    public static class MyExtensions
+    {
+    }
+}
