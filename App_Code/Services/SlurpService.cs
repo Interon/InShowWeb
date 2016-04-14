e2f4f6ea9fbac4981fcd65d25fda66b6f71a63a8
Anton Oosthuizen <anton@interon.co.za>
2016-04-14 11:54:37 +0200

    Server Update and Slurpy

---
+++ App_Code/Services/SlurpService.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+
+/// <summary>
+/// Summary description for SlurpService
+/// </summary>
+public class SlurpService
+{
+    public SlurpService(string url)
+    {
+        GetAllImages(url);
+    }
+    private List<string> imageList = new List<string>();
+    public List<string> DivList = new List<string>();
+    String innerHtml = "";
+
+    public List<string> ImageList
+    {
+        get
+        {
+            return imageList;
+        }
+
+        set
+        {
+            imageList = value;
+        }
+    }
+
+    public string InnerHtml
+    {
+        get
+        {
+            return innerHtml;
+        }
+
+        set
+        {
+            innerHtml = value;
+        }
+    }
+
+    public void GetAllImages(string url)
+    {
+        // Declaring 'x' as a new WebClient() method
+        WebClient x = new WebClient();
+        // Setting the URL, then downloading the data from the URL.
+        string source = x.DownloadString(url);
+        // Declaring 'document' as new HtmlAgilityPack() method
+        HtmlAgilityPack.HtmlDocument document = new HtmlAgilityPack.HtmlDocument();
+        // Loading document's source via HtmlAgilityPack
+        document.LoadHtml(source);
+        // For every tag in the HTML containing the node img.
+        foreach (var link in document.DocumentNode
+                             .Descendants("img")
+                             .Select(s => s.Attributes["src"]))
+        {
+            // Storing all links found in an array. You can declare this however you want.]
+            try
+            {
+                ImageList.Add(link.Value);
+            }
+            catch
+            { }
+
+           
+        }
+        foreach (var content in document.DocumentNode
+                             .Descendants("div")
+                             .Select(s => s.Attributes["class"] ))
+        {
+            // Storing all links found in an array. You can declare this however you want.]
+            try
+            {
+                if(content.Value == "p24_details")
+                {
+                    InnerHtml = content.OwnerNode.InnerHtml;
+                }
+               
+            }
+            catch
+            { }
+
+
+        }
+    }
+
+    
+
+}
